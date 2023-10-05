5cf6245f96461824ae5103ecf6bd019c353bdca0
devonfox <devonhouston.fox@gmail.com>
2023-10-04 20:47:20 -0700

    testing openai gpt3.5 api

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
 import * as readlineSync from 'readline-sync';
+import OpenAI from "openai";
 
-function main() {
-  const name = readlineSync.question('What is your name? ');
-  console.log(`Hello, ${name}! Welcome to my TypeScript Node.js app.`);
+async function run() {
+//   const name = readlineSync.question('\nWhat is your name? ');
+//   console.log(`Hello, ${name}! Welcome to my TypeScript Node.js app.`);
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: "Say this is a test" }],
+        model: "gpt-3.5-turbo",
+    });
+    console.log("AI's response:", chatCompletion.choices[0].message.content);
 }
 
-main();
+run();
